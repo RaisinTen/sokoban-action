79b614d4d111873d0dcd0bb7f199b759c1b4b3ab
raisinten <raisinten@gmail.com>
2020-09-13 18:08:35 +0530

    Bug fix: repoToken

--- .github/actions/sokoban/index.js
+++ .github/actions/sokoban/index.js
@@ -3,13 +3,13 @@ const github = require("@actions/github");
 
 async function run() {
     try {
-        const octokit = github.getOctokit(token);
-
         const issueNumber = core.getInput("issue-number");
         const issueUser = core.getInput("issue-user");
         const move = core.getInput("move");
         const repoToken = core.getInput("repo-token");
 
+        const octokit = github.getOctokit(repoToken);
+        
         octokit.issues.createComment({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,

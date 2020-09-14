6f6dfe6511efa0df5f7ecfa592a0edb1d1a77e47
raisinten <raisinten@gmail.com>
2020-09-14 19:24:06 +0530

    updateReadmeString added

--- .github/actions/sokoban/index.js
+++ .github/actions/sokoban/index.js
@@ -243,7 +243,109 @@ class Game {
 
     // updates README.md
     updateReadme = () => {
-        ;
+
+        console.log("updateReadme called");
+
+        const gameString = "<table>
+  <tr>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/block.png">
+    </td>
+    <td>
+      <img src="./images/floor.png">
+    </td>
+    <td>
+      <img src="./images/block.png">
+    </td>
+    <td>
+      <img src="./images/floor.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/characterOnGoal.png">
+    </td>
+    <td>
+      <img src="./images/goal.png">
+    </td>
+    <td>
+      <img src="./images/reached.png">
+    </td>
+    <td>
+      <img src="./images/floor.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+    <td>
+      <img src="./images/wall.png">
+    </td>
+  </tr>
+</table>";
+
+        const finalReadmeString =
+`# Sokoban Action
+
+${ gameString }
+
+<h1>
+  <a href="https://github.com/RaisinTen/sokoban-action/issues/new?title=$U&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.">:arrow_up:</a>
+  <a href="https://github.com/RaisinTen/sokoban-action/issues/new?title=$D&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.">:arrow_down:</a>
+  <a href="https://github.com/RaisinTen/sokoban-action/issues/new?title=$R&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.">:arrow_right:</a>
+  <a href="https://github.com/RaisinTen/sokoban-action/issues/new?title=$L&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.">:arrow_left:</a>
+  <a href="https://github.com/RaisinTen/sokoban-action/issues/new?title=$B&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.">:leftwards_arrow_with_hook:</a>
+</h1>`;
+
+        console.log("Final Readme string:");
+        console.log(finalReadmeString);
+
+        fs.writeFileSync("./README.md", finalReadmeString);
     }
 
     // updates game.state, game.moves and README.md

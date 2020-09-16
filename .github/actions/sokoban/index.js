959c4d589f0453d08d6324bd30f89c4f681070c1
raisinten <raisinten@gmail.com>
2020-09-16 14:37:40 +0530

    Updated Readme string

--- .github/actions/sokoban/index.js
+++ .github/actions/sokoban/index.js
@@ -411,7 +411,15 @@ ${ gameString }
   <a href="https://github.com/RaisinTen/sokoban-action/issues/new?title=$R&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.">:arrow_right:</a>
   <a href="https://github.com/RaisinTen/sokoban-action/issues/new?title=$L&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.">:arrow_left:</a>
   <a href="https://github.com/RaisinTen/sokoban-action/issues/new?title=$B&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.">:leftwards_arrow_with_hook:</a>
-</h1>`;
+</h1>
+
+## Instructions
+
+Please refer to the [instructions](INSTRUCTIONS.md) to know how this works.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).`;
 
         console.log("Final Readme string:");
         console.log(finalReadmeString);
